9dc0f254dded549fc940b92291ca004d7ad1d8c0
shodson02 <98664424+shodson02@users.noreply.github.com>
2024-04-04 21:13:01 -0400

    PreRelease 0.0.0 --> models.py (83)

--- models.py
+++ models.py
@@ -80,7 +80,7 @@ class User(UserMixin, db.Model):
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.query.get(int(1))
 
 # create a table schemas
 
