a20a87319cb20b2bf869acbc2158ed5d545b1bb5
shodson02 <98664424+shodson02@users.noreply.github.com>
2024-05-21 18:06:53 -0400

    Updated GitHub URL

--- static/index.js
+++ static/index.js
@@ -337,7 +337,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
             drops[i]++;
 
-            // reset position
+            // reset drop position
             if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
                 drops[i] = 0;
             }
